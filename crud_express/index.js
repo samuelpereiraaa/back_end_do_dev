2d97217affe4f0be28767a2a2f0a7e2acddd72b0
samuel <samuelpereira.dev@gmail.com>
2025-12-18 17:03:41 -0300

    manipulating_data_with_express

--- crud_express/index.js
+++ crud_express/index.js
@@ -1,42 +1,80 @@
-const express = require("express")
-const app = express()
+    const express = require("express")
+    const app = express()
 
-app.use(express.json())
+    app.use(express.json())
+
+    const users = []
  
     // get for all
     app.get("/users", (req, res) => {
-    console.log('start get')    
-    res.status(200).send("get completed!")
+    console.log('start get for all')    
+    res.status(200).send({users :users})
     })
 
     //get for id 
     app.get("/users/:id/", (req, res) => {
-        console.log('searh for id...')
-        console.log(req.params.id)
-        res.status(200).send('searh for id completed!')
+        
+        const userID = req.params.id
+        const user = users.find(x => x.id == userID)
+
+        if(user){
+            res.status(200).json(user)
+            console.log('searh for id completed')
+        }
+        else{
+            res.status(404).send("user not found!!")
+
+        }
     })
 
     // post
     app.post("/users", (req, res) => {
         console.log(req.body)
+        users.push(req.body)
         console.log('start post...')
-        res.status(200).send(req.body)
+        res.status(201).send(req.body)
     })
 
     // put for id
     app.put("/users/:id", (req,res) => {
-        console.log("start  put...")
-        res.status(200).send(req.params.id)
-    })
+        const alunoId = req.params.id
+        const updateData = req.body
 
+        // search index
+        const index = users.findIndex(y => y.id == alunoId)
+
+        // update for body
+        if (index !== -1){
+            users[index] = {
+                id: alunoId,
+                nome: updateData.nome,
+                idade: updateData.idade,
+                email: updateData.email
+            }
+            res.status(200).json(users[index])
+            console.log("put success")
+            return
+        } else {
+            res.status(404).json({notification : "ERROR, object not found" })
+            return
+        }
+    })
     // delete for id
-    app.delete("/users/:id", () => {
-        console.log("start delete")
-        res.status(200).send(req.params.id)
+    app.delete("/users/:id", (req, res) => {
+        const alunoId = req.params.id
+        const index = users.findIndex(d => d.id == alunoId)
+
+        if(index !== -1){
+            users.splice(index,1)
+            res.status(202).json({notification: `client of id: ${alunoId}, deleted!`})
+            return
+        }
+        else{
+            res.status(404).send({notification: `id ${alunoId} not found !!`})
+
+        }
+
     })
-
-
-
-app.listen(3001, () => {
+    app.listen(3001, () => {
     console.log('init server')
-})
+    })
